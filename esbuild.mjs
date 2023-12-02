39539b503213962b527b7fb1a7adfa588c4a3816
michael mueller <37136025+michmue@users.noreply.github.com>
2023-12-02 11:21:12 +0100

    added: npm run serve

--- esbuild.mjs
+++ esbuild.mjs
@@ -16,8 +16,9 @@ let ctx = await esbuild.context({
     format: 'esm',
     sourcemap: "both",
     target: "es2021",
-    // sourceRoot: "http://localhost:8000/"
-    // outfile: 'out.js',
+	banner: {
+    js: '//comment',
+  },
 });
 
 let {host, port} = await ctx.serve({
@@ -25,53 +26,3 @@ let {host, port} = await ctx.serve({
 });
 
 console.log(`http://localhost:${port}/`);
-
-
-
-// console.log(entryPoints);
-// let c = `// ==UserScript==
-// // @name\t\t\t\t\t\tDevelopment
-// // @version 1
-// // @match\t\t\t\t\t\t*://hanime.tv/videos/hentai/*
-// // @match\t\t\t\t\t\t*://hanime.tv/omni-player/*
-// // @match\t\t\t\t\t\t*://player.hanime.tv/*
-// //
-// // @allFrames true
-// // @run-at document-end
-// // @grant   GM.fetch
-// // ==/UserScript==
-// `;
-
-// /**
-//  * @param {string} entryPoint
-//  * @param {string} metadata
-//  */
-// function addUserScriptMetaData(entryPoint, metadata) {
-//     var fs = require('fs');
-//     var data = fs.readFileSync('./example.js').toString().split("\n");
-//     data.splice(0, 0, "Append the string whatever you want at top" );
-//     var text = data.join("\n");
-//
-//     fs.writeFile('./example.js', text, function (err) {
-//         if (err) return err;
-//     });
-// }
-//
-// for (const entryPoint of entryPoints) {
-//     let inputStream = fs.createReadStream(entryPoint);
-//     let lineReader = readline.createInterface({
-//         input: inputStream
-//     });
-//
-//     let metadata = "";
-//     lineReader.on('line', function (line) {
-//         if (line.includes("==/UserScript==")) {
-//             lineReader.removeAllListeners('line', this);
-//             lineReader.close();
-//             inputStream.close();
-//             addUserScriptMetaData(entryPoint, metadata);
-//             metadata = "";
-//         }
-//         console.log('Line from file:', line);
-//     });
-// }
